b850da633cca40559a61eda0ca8ccb44149e93ce
Ferhan Akkan <ferhanakkan@gmail.com>
2020-04-04 12:00:54 +0300

    Initial Commit

---
+++ MultiLanguageApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultiLanguageApp
+//
+//  Created by Ferhan Akkan on 4.04.2020.
+//  Copyright © 2020 Ferhan Akkan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
